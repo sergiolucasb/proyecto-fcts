5b91ca1a640617dd9057ff3d2aa44f7e905cbe40
Sergio Lucas Ballester <sergiolucasballester@gmail.com>
2024-05-05 21:33:15 +0200

    creado_listado_empresas_footer

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
                 <p>GESTION FCTs EFA EL CAMPICO</p>
             </div>
             <div>
-                <a href="#">Mis empresas</a>
+                <a href="mis_empresas.php">Mis empresas</a>
                 <a href="#">Editar perfil</a>
                 <a href="#">Salir</a>
             </div>
@@ -79,10 +79,18 @@
                     echo "<p>".$row['telefono']."</p>";
                     echo "<a href='comterios.php'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill='#000000' d='M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z'/></svg></a>";
                     echo "<p>Preferencia</p>";
+                    echo "</div>";
                 }
             ?>
-            
         </article>
     </section>
+    <footer>
+        <p>FCTs EFA El Campico</p>
+        <div>
+            <a href="#">Contacto</a>
+            <a href="https://www.elcampico.org/">Mi centro</a>
+        </div>
+        <img src="img/logo-el-campico.png" alt="Logo EFA El Campico">
+    </footer>
 </body>
 </html>
