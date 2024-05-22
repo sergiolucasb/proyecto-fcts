cf3af498201e8d58c8d4d45868dbe9927ef4faaa
Sergio Lucas Ballester <sergiolucasballester@gmail.com>
2024-05-22 21:18:21 +0200

    Añadido CSS

--- nueva_empresa.php
+++ nueva_empresa.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="nueva_empresa.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <title>Añadir empresa</title>
     <?php
     $host = 'localhost';
@@ -42,9 +46,16 @@
     $datos[':num_plazas'] = $num_plazas;
     $datos[':telefono'] = $telefono;
     $datos[':persona_contacto'] = $persona_contacto;
+
+    $error = false;
     if (!empty($anyadir)) {
-        $consulta = $pdo->prepare($sql);
-        $consulta->execute($datos);
+        if (!empty($nombre_empresa)) {
+            $consulta = $pdo->prepare($sql);
+            $consulta->execute($datos);
+        } else {
+            $error = true;
+        }
+
     }
     
 
@@ -52,6 +63,12 @@
 </head>
 <body>
     <form action="nueva_empresa.php" method="post">
+        <h2>Nueva empresa</h2>
+        <?php
+        if ($error == true) {
+            echo "<p class='error'>Error: El nombre de la empresa no puede estar vacío</p>";
+        }
+        ?>
         <input type="text" placeholder="Nombre de la empresa..." name="nombre_empresa">
         <input type="text" placeholder="CIF..." name="cif">
         <input type="text" name="nombre_fiscal" placeholder="Nombre fiscal...">
@@ -62,14 +79,10 @@
         <input type="number" name="num_plazas" placeholder="Número de plazas...">
         <input type="number" name="telefono" placeholder="Teléfono...">
         <input type="text" name="persona_contacto" placeholder="Persona de contacto...">
-        <input type="submit" value="Añadir" name="anyadir">
-
-
-
-
-
-
-
+        <div>
+            <input type="submit" value="Añadir" name="anyadir">
+            <a href="gestion_empresas.php">Volver</a>
+        </div>
 
     </form>
 </body>
