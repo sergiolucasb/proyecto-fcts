e807f549e98eadb0b2d57c2ade4ae56bcee30adc
Sergio Lucas Ballester <sergiolucasballester@gmail.com>
2024-05-23 16:56:46 +0200

    Diseñado botón modificar

--- gestion_empresas.php
+++ gestion_empresas.php
@@ -111,7 +111,7 @@
         </nav>
     </header>
     <section>
-        <h2>Listado de empresas</h2>
+        <h2>Gestión de empresas</h2>
         <form action="gestion_empresas.php" method="post">
             <article id="filtros_busqueda">
                 <input type="text" placeholder="Buscar por nombre" name="nombre_empresa">
@@ -123,15 +123,15 @@
                 <div>
                     <p>Nombre de la empresa</p>
                     <p>Contacto</p>
-                    <p>Comentarios</p>
-                    <p>Preferencia</p>
+                    <p>Modificar</p>
+                    <p>Eliminar</p>
                 </div>
                 <?php
                 while ($row = $consulta->fetch()) {
                     echo "<div>";
                     echo "<p>" . $row['nombre'] . "</p>";
                     echo "<p>" . $row['telefono'] . "</p>";
-                    echo "<a href='#'><svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill='#000000' d='M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z'/></svg></a>";
+                    echo "<a href='nueva_empresa.php'>Modificar</a>";
                     echo "<select name='preferencia'>";
                     echo "<option value='1'>1</option>";
                     echo "<option value='2'>2</option>";
