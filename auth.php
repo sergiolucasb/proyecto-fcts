c2e2c49aac1f637f6c2884603fa289a035a05fcb
Alvaro Rodriguez Sanchez <alvarorodriguezsanchez326@gmail.com>
2024-05-29 20:17:56 +0200

    Añadido exit() en el auth.php, por posible conflicto

--- auth.php
+++ auth.php
@@ -1,11 +1,8 @@
 <?php
-    //inicio sesion
     session_start();
-    //verifico que el usuario este logueado  y con isset verifico si ese campo esta vacio que es el campo porque el registro campo unico 
     if(!isset($_SESSION['email'])){
-        //si no esta logueado lo redirijo al login
         header("Location:login.php");
-        //destruye la sesion cuando acabe
         session_destroy();
+        exit();
     }
 ?>
